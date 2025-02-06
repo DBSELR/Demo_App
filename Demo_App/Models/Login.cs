2dca26ad56225d98ed9eec289df83ddd3c2ffc7e
DBSELR <89074505+DBSELR@users.noreply.github.com>
2025-02-06 17:45:49 +0530

    Ramesh

---
+++ Demo_App/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_App.Models
+{
+    public class Login
+    {
+        public string _Regno;
+        public string _course;
+    }
+}
